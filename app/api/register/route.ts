b1c17fd3eeb7eb7c2db79084c757e7c87f34c2f9
Brian Emilius <be@rts.dk>
2026-02-01 13:06:57 +0100

    refactor: simplify POST function signature and import statement

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,5 +1,5 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export default async function POST(request: NextRequest) {
+export async function POST(request: Request) {
 	return NextResponse.json({});
 }
