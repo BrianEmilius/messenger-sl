3353c02cad2b0b427cb4f1629f31aade8d457dbf
Brian Emilius <be@rts.dk>
2026-02-01 13:56:01 +0100

    feat: implement agent registration with database integration and error handling

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,7 +1,26 @@
+import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
 	const body = await request.json();
-	console.log(body);
-	return NextResponse.json({});
+	console.log("Register request recevied");
+
+	const agents = body.map((agent: string) => ({
+		key: agent
+	}));
+
+	console.log("creating DB entries");
+
+	try {
+		const createdAgents = prisma.agent.createMany({
+			data: agents
+		});
+
+		console.log("Created agents", createdAgents);
+
+		return NextResponse.json({ success: true });
+	} catch (error) {
+		console.log("error", error);
+		return NextResponse.json({ success: false });
+	}
 }
