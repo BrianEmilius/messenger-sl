4e96216dc9e3ced38199dae091131330dc1fb779
Brian Emilius <be@rts.dk>
2026-02-01 14:59:41 +0100

    refactor: remove console log and tidy up agent rendering in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
+import { Agent } from "@/generated/prisma/client";
 import { prisma } from "@/lib/prisma";
 
 export default async function Home() {
   const agents = await prisma.agent.findMany();
-  console.log("agents on home page", agents);
   return (
     <>
       <h1>Home</h1>
-
+      {agents.map((agent: Agent) => <p key={agent.id}>{agent.key}</p>)}
     </>
   );
 }
